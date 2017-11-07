55d56e8053b2606593690b3501f2f5b71db2e8a7
pweids <paul.weidinger@gmail.com>
2017-11-06 19:03:36 -0500

    returning httpresponse with expected html

--- budget/tests.py
+++ budget/tests.py
@@ -1,7 +1,20 @@
 from django.test import TestCase
+from django.urls import resolve
+from django.http import HttpRequest
+
+from budget.views import home_page
 
 
-class SmokeTest(TestCase):
+class HomePageTest(TestCase):
 
-    def test_bad_math(self):
-        self.assertEqual(1 + 1, 3)
+    def test_root_url_resolves_to_home_page_view(self):
+        found = resolve('/')
+        self.assertEqual(found.func, home_page)
+
+    def test_home_page_returns_correct_html(self):
+        request = HttpRequest()
+        response = home_page(request)
+        html = response.content.decode('utf8')
+        self.assertTrue(html.startswith('<html>'))
+        self.assertIn('<title>pmoney</title>', html)
+        self.assertTrue(html.endswith('</html>'))
