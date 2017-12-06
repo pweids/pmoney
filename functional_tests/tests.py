1daa69e7811f70070c85ada7aa7d3dc12105c6d0
pweids <paul.weidinger@gmail.com>
2017-12-05 21:13:26 -0500

    changing functional tests to LiveServerTestCase

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -1,32 +1,23 @@
 from selenium import webdriver
-import unittest
+from django.contrib.auth.models import User
+from django.test import LiveServerTestCase
 import time
 
 
-class PaulVisitor(unittest.TestCase):
+class PaulVisitor(LiveServerTestCase):
 
     def setUp(self):
         self.browser = webdriver.Firefox()
+        self.user = User.objects.create_user("pweids", "p@p.com", "pmoney")
+        self.user.save()
 
     def tearDown(self):
         self.browser.quit()
 
-    def submit_login_form(self, username, password):
-        login_form = self.browser.find_element_by_id('id_login_form')
-        username_box = self.browser.find_element_by_id("id_username")
-        password_box = self.browser.find_element_by_id('id_password')
-        self.assertEqual(
-            password_box.get_attribute('type'),
-            'password')
-        username_box.send_keys(username)
-        password_box.send_keys(password)
-        login_form.submit()
-        time.sleep(1)
-
     def test_can_view_list_and_add_or_edit_items(self):
         # Paul wants to see his remaining monthly expenditures,
         # so he visits his own site pmoney.me
-        self.browser.get('http://localhost:8000')
+        self.browser.get(self.live_server_url)
 
         # He notices the header tells him where he is
         header_text = self.browser.find_element_by_tag_name('h1').text
@@ -54,7 +45,7 @@ class PaulVisitor(unittest.TestCase):
         title_text = self.browser.find_element_by_tag_name('h1').text
         self.assertIn('December', title_text)
         remaining_text = self.browser.find_element_by_id("id_remaining").text
-        self.assertIn('$1,521', text)
+        self.assertIn('$1,521', remaining_text)
 
         # He also sees a figure that tells him how much money
         # he has left to spend per day, including an estimate of
@@ -96,6 +87,14 @@ class PaulVisitor(unittest.TestCase):
         # Satisfied with his finacial situation, he logs out and goes to sleep
         self.fail("Finish the test!")
 
-
-if __name__ == "__main__":
-    unittest.main(warnings='ignore')
+    def submit_login_form(self, username, password):
+        login_form = self.browser.find_element_by_id('id_login_form')
+        username_box = self.browser.find_element_by_id("id_username")
+        password_box = self.browser.find_element_by_id('id_password')
+        self.assertEqual(
+            password_box.get_attribute('type'),
+            'password')
+        username_box.send_keys(username)
+        password_box.send_keys(password)
+        login_form.submit()
+        time.sleep(1)
